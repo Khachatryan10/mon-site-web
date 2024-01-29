f6c7e1536d1ba1a524e5aac7dba4b15a5d90a9c1
Khachatryan10 <khchokhachatryan433@gmail.com>
2024-01-29 19:02:25 +0100

    image size changed and text corrected and navbar size changed on mobile size

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -7,9 +7,9 @@ export default function About() {
             <h2 className="aboutDiv__h2">Typescript and Python Developer</h2>
             <div className="aboutDiv__div">
                 <p>
-                    Hello World, my name is Khachatur and my family name is Khachatryan, i live in France, and I'm a freelancer.
-                    I started learning programming from the end of 2021 and now I have 7 certificates from CS50 and FreeCodeCamp and 5 projects,
-                    3 of which are full stack. I have experience with the following technologies Python, Javascript,
+                    Hello World, my name is Khachatur and my family name is Khachatryan, i live in France, and i'm a freelancer.
+                    I started learning programming from the end of 2021 and now I have 7 certificates from CS50 and FreeCodeCamp and 5 personal projects,
+                    3 of which are Full Stack. I have experience with the following technologies Python, Javascript,
                     Typescript, HTML, CSS, Sass, React, Redux, Django, SQL, basics of Git, basics of D3.js, npm with which i can work professionally
                     and i currently learn React Native with which i have a project on this website, however it's not fully completed yet.
                     I also have experience with C, Scratch, Flask, basics of AWS, Bootstrap, jQuery (currently not working with them professionally yet).
