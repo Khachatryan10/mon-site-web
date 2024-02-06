2787d7de26105d568861dce59e658762e2e239ec
Khachatryan10 <khchokhachatryan433@gmail.com>
2024-02-06 22:41:12 +0100

    commited navbar url change

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -6,10 +6,10 @@ export default function Navbar() {
     return (
         <nav className="nav">
             <ul>
-                <Link to={"certifications"}><li className={location.pathname === "/certifications" ? "currentlyOn": ""}>Certifications</li></Link>
-                <Link to={"projects"}><li className={location.pathname === "/projects" ? "currentlyOn": ""}>Projects</li></Link>
-                <Link to={"contact"}><li className={location.pathname === "/contact" ? "currentlyOn": ""}>Contact</li></Link>
-                <Link to={"about"}><li className={location.pathname === "/about" ? "currentlyOn": ""}>About</li></Link>
+                <Link to={"/certifications"}><li className={location.pathname === "/certifications" ? "currentlyOn": ""}>Certifications</li></Link>
+                <Link to={"/projects"}><li className={location.pathname === "/projects" ? "currentlyOn": ""}>Projects</li></Link>
+                <Link to={"/contact"}><li className={location.pathname === "/contact" ? "currentlyOn": ""}>Contact</li></Link>
+                <Link to={"/about"}><li className={location.pathname === "/about" ? "currentlyOn": ""}>About</li></Link>
             </ul>
         </nav>
     )
