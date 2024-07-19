ce767718079865068e13fb6ab4d4ef52fed8e646
Khachatryan10 <khchokhachatryan433@gmail.com>
2024-07-19 14:04:18 +0200

    new certificate added (Polimi)

--- src/data/certificates-data.ts
+++ src/data/certificates-data.ts
@@ -5,40 +5,46 @@ export const certificatesData: DataI[] = [
         "id": "1",
         "name": "",
         "description": "",
-        "images": ["./certificates/cs50cy.png"]
+        "images": ["./certificates/Polimi.png"]
     },
     {
         "id": "2",
         "name": "",
         "description": "",
-        "images": ["./certificates/cs50sql.png"]
+        "images": ["./certificates/cs50cy.png"]
     },
     {
         "id": "3",
         "name": "",
         "description": "",
-        "images": ["./certificates/cs50w.png"]
+        "images": ["./certificates/cs50sql.png"]
     },
     {
         "id": "4",
         "name": "",
         "description": "",
-        "images": ["./certificates/cs50t.png"]
+        "images": ["./certificates/cs50w.png"]
     },
     {
         "id": "5",
         "name": "",
         "description": "",
-        "images": ["./certificates/cs50x.png"]
+        "images": ["./certificates/cs50t.png"]
     },
     {
         "id": "6",
         "name": "",
         "description": "",
-        "images": ["./certificates/fcc2.png"]
+        "images": ["./certificates/cs50x.png"]
     },
     {
         "id": "7",
+        "name": "",
+        "description": "",
+        "images": ["./certificates/fcc2.png"]
+    },
+    {
+        "id": "8",
         "name": "",
         "description": "",
         "images": ["./certificates/fcc1.png"]
