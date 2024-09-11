a685ebffd47a78fdcc65ee060ce0d74a9c716bc0
Khachatryan10 <khchokhachatryan433@gmail.com>
2024-09-11 21:52:50 +0200

    modified about

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -7,13 +7,13 @@ export default function About() {
             <h2 className="aboutDiv__h2">Typescript and Python Developer</h2>
             <div className="aboutDiv__div">
                 <p>
-                    Hello World, my name is Khachatur and my family name is Khachatryan and i live in France.
-                    I started learning programming from the end of 2021 and now I have 8 certificates from CS50, FreeCodeCamp and Polimi Open Knowledge. I also have 5 personal projects,
-                    3 of which are Full Stack. I have experience with the following technologies Python, Javascript,
-                    Typescript, HTML, CSS, Sass, React, Redux, Django, SQL, basics of Git, basics of D3.js, npm with which i can work professionally.
-                    I currently learn React Native with which i have a project on this website, however it's not fully completed yet.
-                    I also have experience with C, Scikit-learn, TensorFlow, Scratch, Flask, basics of AWS, Bootstrap, jQuery (currently not working with them professionally).
-                    In this website you can find my projects and certifications in order to see my skills put into practice.
+                    Hello World, my name is Khachatur and my family name is Khachatryan, i live in France.
+                    I started learning programming from the end of 2021 and earned 8 certificates from CS50, FreeCodeCamp and Polimi Open Knowledge and also built 5 personal projects (3 of which are Full Stack) along the way.
+                    I am proficient in the following technologies: Python, Javascript,
+                    Typescript, HTML, CSS, Sass, React, Redux, Django, SQL, basics of Git, basics of D3.js, npm.
+                    I currently learn React Native with which there is a project posted on this website.
+                    I also have experience with: C, Scikit-learn, TensorFlow, Scratch, Flask, basics of AWS, Bootstrap, jQuery (currently not working with them professionally).
+                    In this website you can find my projects and certifications as demonstration of my acquired skills.
                 </p>
             </div>
         </div>
