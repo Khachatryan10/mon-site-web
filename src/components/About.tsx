d95b676f7c13afed150f58fd12663573b732a62f
Khachatryan10 <khchokhachatryan433@gmail.com>
2024-09-11 22:02:43 +0200

    modified about

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -10,9 +10,9 @@ export default function About() {
                     Hello World, my name is Khachatur and my family name is Khachatryan, i live in France.
                     I started learning programming from the end of 2021 and earned 8 certificates from CS50, FreeCodeCamp and Polimi Open Knowledge and also built 5 personal projects (3 of which are Full Stack) along the way.
                     I am proficient in the following technologies: Python, Javascript,
-                    Typescript, HTML, CSS, Sass, React, Redux, Django, SQL, basics of Git, basics of D3.js, npm.
+                    Typescript, HTML, CSS, Sass, React, Redux, Django, SQL.
                     I currently learn React Native with which there is a project posted on this website.
-                    I also have experience with: C, Scikit-learn, TensorFlow, Scratch, Flask, basics of AWS, Bootstrap, jQuery (currently not working with them professionally).
+                    I also have experience with: C, Scikit-learn, TensorFlow, Scratch, Flask, basics of AWS, Bootstrap, jQuery and D3.js (currently not working with them professionally).
                     In this website you can find my projects and certifications as demonstration of my acquired skills.
                 </p>
             </div>
